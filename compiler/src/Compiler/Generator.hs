a840ac3f307aaeaad9c58e79e4cb41e87c581cc3
Julian Loscombe <julian.loscombe@gmail.com>
2016-10-10 23:05:11 +0100

    Added function declarations for Generator.hs

--- compiler/src/Compiler/Generator.hs
+++ compiler/src/Compiler/Generator.hs
@@ -4,3 +4,36 @@ module Compiler.Generator
 
 import Compiler.SyntaxTree
 import Assembly.Instruction
+
+generateProgram :: [ Function ] -> [ Instruction ]
+generateProgram = undefined
+
+generateFunction :: Function -> [ Instruction ]
+generateFunction = undefined
+
+generateStatement :: Statement -> [ Instruction ]
+generateStatement = undefined
+
+generateDecVar :: DecVar -> [ Instruction ]
+generateDecVar = undefined
+
+generateAssVar :: AssVar -> [ Instruction ]
+generateAssVar = undefined
+
+generateAssign :: Assign -> [ Instruction ]
+generateAssign = undefined
+
+generateAssignDecl :: AssignDecl -> [ Instruction ]
+generateAssignDecl = undefined
+
+generateFuncCall :: FuncCall -> [ Instruction ]
+generateFuncCall = undefined
+
+generateExpression :: Expression -> [ Instruction ]
+generateExpression = undefined
+
+generateAexp :: Aexp -> [ Instruction ]
+generateAexp = undefined
+
+generateBexp :: Bexp -> [ Instruction ]
+generateBexp = undefined
